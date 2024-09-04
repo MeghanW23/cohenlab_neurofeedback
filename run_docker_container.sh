724abcbcff7f5ed59fd92c33fa782af8401e23d9
MeghanW23 <wmeghan0809@gmail.com>
2024-09-04 16:38:34 -0400

    edit run_docker_container.sh

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,3 +1,8 @@
 #!/bin/bash 
+
+echo "Pulling any repo changes"
+git pull origin main 
+
+echo "Running Docker"
 docker run -it --rm -v "$(pwd):/workdir" nfb_docker:1.0 /bin/bash
 # docker run -it nfb_docker:1.0 /bin/bash
