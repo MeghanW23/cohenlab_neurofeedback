54f63a136193be7745c5d7bb825552f09e38fbf4
MeghanW23 <wmeghan0809@gmail.com>
2024-09-06 16:48:00 -0400

    edit startup script

--- startup_docker.sh
+++ startup_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# set up any aliases needed during the container runtime
 echo "alias rifg='python /workdir/run_docker/rifg_task/rifg_task.py'" >> ~/.bashrc
 source ~/.bashrc
 exec "$@"
