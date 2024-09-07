18418b3076ea8720e656dfc3db0fa7c081686021
MeghanW23 <wmeghan0809@gmail.com>
2024-09-07 16:54:24 -0400

    organize

---
+++ tasks_run/scripts/main.py
@@ -0,0 +1,3 @@
+from log import logger
+
+output_log = logger(action="create", filetype=".csv", log_name="test")
---
+++ tasks_run/scripts/settings.py
@@ -0,0 +1,7 @@
+SAMBASHARE_DIR_PATH: str = "/workdir/tasks_run/data/sambashare"
+ROI_MASK_PATH: str = "/workdir/tasks_run/data/roi_masks"
+LOGGING_DIR_PATH: str = "/workdir/tasks_run/data/logs"
+
+BLOCK_NUM: int = 1
+N_TRIALS: int = 140
+RETRIES_BEFORE_ENDING: int = 3
