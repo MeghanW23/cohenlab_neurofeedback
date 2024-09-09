f6c092f9bb7de493da13e693fb0535c916181b3e
MeghanW23 <wmeghan0809@gmail.com>
2024-09-09 15:15:39 -0400

    edit startup script aliases, make mock script

--- startup_docker.sh
+++ startup_docker.sh
@@ -5,7 +5,9 @@ echo "alias rifg='python /workdir/run_docker/rifg_task/rifg_task.py'" >> ~/.bash
 echo "alias calculate='python /workdir/tasks_run/scripts/main.py'" >> ~/.bashrc
 echo "alias data='cd /workdir/tasks_run/data/'" >> ~/.bashrc
 echo "alias logs='cd /workdir/tasks_run/data/logs'" >> ~/.bashrc
-echo "alias scripts='cd /workdir/tasks_run/data/scripts'" >> ~/.bashrc
+echo "alias scripts='cd /workdir/tasks_run/scripts'" >> ~/.bashrc
+echo "alias samba='cd /workdir/tasks_run/data/sambashare'" >> ~/.bashrc
+echo "alias mock='python /workdir/tasks_run/scripts/mock_mri_file_production.py'" >> ~/.bashrc
 
 source ~/.bashrc
 exec "$@"
