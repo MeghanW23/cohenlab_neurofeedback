631d4a70f51fc16e05a22d38ed0fedb779befd5a
sofheras <sofiaheras@osxlap11684.tch.harvard.edu>
2024-09-12 16:32:04 -0400

    add rest period to rifg task

---
+++ old_material/make_bad_dicoms.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+import subprocess
+
+error_dicom_dir: str = "/tasks_run/data/sambashare/error_testing_dcms/"
+list_deleted_files_path: str = "/Users/meghan/cohenlab_neurofeedback/tasks_run/data/sambashare/error_testing_dcms/deleted_dicoms.txt"
+
+with open(list_deleted_files_path, 'w') as file_log:
+    file_log.write("This File Contains the List of Filenames for Files That Will Cause a dcm2niix Error")
+
+deleted_files: list = []
+count: int = 0
+for file in sorted(os.listdir(error_dicom_dir)):
+    file_path: str = os.path.join(error_dicom_dir, file)
+    if "80" in file_path:
+        count += 1
+
+        print(file_path)
+
+        deleted_files.append(file_path)
+
+        subprocess.run(['rm', '-f', file_path])
+        subprocess.run(['touch', file_path])
+
+        with open(list_deleted_files_path, 'a') as file_log:
+            file_log.write(file_path)
+        
+print(f"total files: {count}")
--- startup_docker.sh
+++ startup_docker.sh
@@ -6,7 +6,7 @@ echo "alias data='cd /workdir/tasks_run/data/'" >> ~/.bashrc
 echo "alias logs='cd /workdir/tasks_run/data/logs'" >> ~/.bashrc
 echo "alias scripts='cd /workdir/tasks_run/scripts'" >> ~/.bashrc
 echo "alias samba='cd /workdir/tasks_run/data/sambashare'" >> ~/.bashrc
-echo "alias mock='python /workdir/tasks_run/scripts/mock_mri_file_production.py'" >> ~/.bashrc
+echo "alias mock='python /workdir/tasks_run/scripts/other/mock_mri_file_production.py'" >> ~/.bashrc
 echo "alias nfb='python /workdir/tasks_run/scripts/nf_calc_MW.py'" >> ~/.bashrc
 
 source ~/.bashrc
