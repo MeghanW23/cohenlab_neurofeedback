1773c369f0e8f518651880f2b5b1fbd7d5a8b00f
MeghanW23 <wmeghan0809@gmail.com>
2024-09-13 13:26:08 -0400

    edits

--- tasks_run/scripts/GUI2.py
+++ tasks_run/scripts/GUI2.py
@@ -14,11 +14,12 @@ class NeurofeedbackGraphicalInterface:
         title = Label(master, text="Neurofeedback Stats and Errors.", font=("Times New Roman", 24, "bold", "underline"))
         title.grid(row=0, column=0, padx=20, pady=10)
 
-        if "output_log_dir" in dictionary["whole_session_data"]:
-            log_path: str = self.dictionary["whole_session_data"]["output_log_dir"]
+        if "output_text_logfile_path" in dictionary["whole_session_data"]:
+            log_path: str = self.dictionary["whole_session_data"]["output_text_logfile_path"]
             Logger.print_and_log(f"GUI Reading from Output File: {log_path}")
 
         else:
-            Logger.print_and_log("GUI Could not find log path")
+            Logger.print_and_log(f"GUI Could not find log path in data dictionary")
+            print(dictionary)
             sys.exit(1)
 
