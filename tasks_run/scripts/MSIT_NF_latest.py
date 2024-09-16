52841c9058ca7987a529258cf992d56461e924b5
sofheras <sofiaheras@osxlap11684.tch.harvard.edu>
2024-09-16 14:38:08 -0400

    interfernce vs control blocks msit

---
+++ tasks_run/scripts/MSIT_NF_latest.py
@@ -0,0 +1,208 @@
+import random
+import time
+import ScriptManager
+import Logger
+import settings
+import Projector
+import pygame
+from datetime import datetime, timedelta
+import pprint
+
+from old_material.MSIT_neurofeedback import CONTROL_BLOCK, INTERFERENCE_BLOCK
+
+
+def generate_series() -> list:
+    series_list: list = []
+
+    for i in range(10):
+        if block_type == CONTROL_BLOCK:
+            target_number = random.randint(1, 3)
+            series = [0,0,0]
+            positions = [0,1,2]
+            random.shuffle(positions)
+            series[positions[0]] = target_number
+
+        elif block_type == INTERFERENCE_BLOCK:
+            same_number = random.randint(1,3)
+            different_number = same_number
+            while different_number == same_number:
+                different_number = random.randint(1,3)
+
+            positions = [0,1,2]
+            random.shuffle (positions)
+
+            series: list = [same_number, same_number, same_number]
+            series[positions[0]] = different_number
+
+        series_list.append(series)
+
+    return series_list
+
+def handle_response(trial_dictionary: dict, screen_width: float, screen_height: float) -> dict:
+    Response = None
+    start_time = pygame.time.get_ticks()
+
+    while Response is None:
+        current_time = pygame.time.get_ticks()
+
+        # Timeout after 3 seconds
+        if current_time - start_time > 3000:
+            Logger.print_and_log("No Response For This Trial")
+            trial_dictionary["response"] = Response
+            trial_dictionary = check_response(trial_dictionary=Data_Dictionary[f"trial{trial}"])
+            break
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            # Key presses mapped to positions
+            if event.type == pygame.KEYDOWN:
+                trial_dictionary["reaction_time"] = datetime.now() - trial_dictionary["start_time"]
+                if event.key == pygame.K_a or event.key == pygame.K_1:  # 'A' key is position 0
+                    Logger.print_and_log("Response: A/1")
+                    Response = 1
+                    trial_dictionary["response"] = Response
+                    trial_dictionary = check_response(trial_dictionary=Data_Dictionary[f"trial{trial}"])
+                    if practice:
+                        show_feedback(trial_dictionary=trial_dictionary, screen_width=screen_width, screen_height=screen_height)
+
+                elif event.key == pygame.K_b or event.key == pygame.K_2:  # 'B' key is position 1
+                    Logger.print_and_log("Response: B/2")
+                    Response = 2
+                    trial_dictionary["response"] = Response
+                    trial_dictionary = check_response(trial_dictionary=Data_Dictionary[f"trial{trial}"])
+                    if practice:
+                        show_feedback(trial_dictionary=trial_dictionary, screen_width=screen_width, screen_height=screen_height)
+
+                elif event.key == pygame.K_c or event.key == pygame.K_3:  # 'C' key is position 2
+                    Logger.print_and_log("Response: C/3")
+                    Response = 3
+                    trial_dictionary["response"] = Response
+                    trial_dictionary = check_response(trial_dictionary=Data_Dictionary[f"trial{trial}"])
+                    if practice:
+                        show_feedback(trial_dictionary=trial_dictionary, screen_width=screen_width, screen_height=screen_height)
+
+    return trial_dictionary
+
+def check_response(trial_dictionary: dict) -> dict:
+    given_number_one: list = []
+    given_number_two: list = []
+    for index, number in enumerate(trial_dictionary["number_series"], start=1):
+        if index == 1:
+            given_number_one.append(number)
+        elif number == given_number_one[0]:
+            given_number_one.append(number)
+        else:
+            given_number_two.append(number)
+        
+    if len(given_number_one) == 1:
+        trial_dictionary["different_number"] = given_number_one[0]
+    elif len(given_number_two) == 1:
+        trial_dictionary["different_number"] = given_number_two[0]
+    else:
+        raise ValueError("Math went wrong, check check_response()")
+
+    Logger.print_and_log(f"Different Number was: {trial_dictionary['different_number']}")
+    if trial_dictionary["different_number"] == trial_dictionary["response"]:
+        trial_dictionary["correct"]: bool = True
+        Logger.print_and_log("Participant was Correct.")
+    else:
+        trial_dictionary["correct"]: bool = False
+        Logger.print_and_log("Participant was Incorrect.")
+
+
+    return trial_dictionary
+
+def show_feedback(trial_dictionary, screen_width, screen_height):
+    if trial_dictionary["correct"]:
+        feedback: str = "correct"
+        color: tuple = (0, 255, 0)
+    else:
+        feedback: str = "incorrect"
+        color: tuple = (255, 0, 0)
+
+
+    feedback_text_surface = feedback_font.render(feedback, True, color)
+    feedback_text_rect: pygame.Rect = feedback_text_surface.get_rect(center=(screen_width // settings.MSIT_SCREEN_DIVISORS_FOR_FEEDBACK[0], screen_height // settings.MSIT_SCREEN_DIVISORS_FOR_FEEDBACK[1]))
+    screen.blit(feedback_text_surface, feedback_text_rect)
+    pygame.display.flip()
+
+
+    time.sleep(settings.MSIT_TIME_TO_SHOW_FEEDBACK)
+
+Data_Dictionary: dict = {'whole_session_data': {}}
+
+pygame.init()
+number_font = pygame.font.Font(None, settings.MSIT_FONT_SIZE_NUMBERS)
+feedback_font = pygame.font.Font(None, settings.MSIT_FONT_SIZE_FEEDBACK)
+random.seed(settings.RANDOM_SEED_VALUE)
+
+# set a fixed seed for reproducibility of a pseudorandom series
+random.seed(settings.RANDOM_SEED_VALUE)
+Data_Dictionary["whole_session_data"]["pid"] = ScriptManager.get_participant_id()
+output_log_path = Logger.create_log(filetype=".txt", log_name=f"{Data_Dictionary['whole_session_data']['pid']}_MSIT")
+# Logger.print_and_log(f"Created Text Output File: {output_log_path}")
+
+practice: str = ""
+while True:
+    practice: str = input("Practice Block? (y/n): ")
+    if practice == 'y':
+        Logger.print_and_log("OK, Will Run This As a Practice Session.")
+        practice: bool = True
+        break
+    elif practice == 'n':
+        Logger.print_and_log("Ok. Not a Practice Session.")
+        practice: bool = False
+        break
+    else:
+        Logger.print_and_log("Please chose either 'y' or 'n'")
+
+block_type = ""
+while block_type not in ["I", "C"]:
+    block_type = input ("Block Type (I/C)?").upper()
+    if block_type == "I":
+        Logger.print_and_log("Interference Block Selected.")
+        block_type = INTERFERENCE_BLOCK
+    elif block_type == "C":
+        Logger.print_and_log("Control Block Selected.")
+        block_type = CONTROL_BLOCK
+    else:
+        Logger.print_and_log("Please choose either 'I' (Interference) or 'C' (Control)")
+
+Data_Dictionary["whole_session_data"]["block_type"] = block_type
+
+Data_Dictionary, screen = Projector.get_monitor_info(dictionary=Data_Dictionary)
+Projector.initialize_screen(screen=screen, instructions=settings.MSIT_INSTRUCTIONS)
+Projector.show_instructions(screen=screen, instructions=settings.MSIT_INSTRUCTIONS)
+Projector.show_fixation_cross_rest(dictionary=Data_Dictionary, screen=screen, Get_CSV_if_Error=True)
+
+series_list: list = generate_series()
+for trial in range(1, settings.MSIT_N_TRIALS + 1):
+    Logger.print_and_log(f"=======Trial{trial}=======")
+    Data_Dictionary[f"trial{trial}"]: dict = {}
+    Data_Dictionary[f"trial{trial}"]["start_time"] = datetime.now()
+    Data_Dictionary[f"trial{trial}"]["block_type"] = block_type
+    screen.fill((0, 0, 0))
+
+    numbers_this_trial: list = random.choice(series_list)
+    Data_Dictionary[f"trial{trial}"]["number_series"]: list = numbers_this_trial
+    
+    numbers_text = f"{numbers_this_trial[0]}  {numbers_this_trial[1]}  {numbers_this_trial[2]}"
+    text_surface = number_font.render(numbers_text, True, (255, 255, 255))
+    screen_width: float = Data_Dictionary["whole_session_data"]["second_monitor_width"]
+    screen_height: float = Data_Dictionary["whole_session_data"]["second_monitor_height"]
+
+    text_rect: pygame.Rect = text_surface.get_rect(center=(screen_width // settings.MSIT_SCREEN_DIVISORS_FOR_NUMBERS[0],  screen_height // settings.MSIT_SCREEN_DIVISORS_FOR_NUMBERS[1]))
+
+    screen.blit(text_surface, text_rect)
+    pygame.display.flip()
+
+    Data_Dictionary[f"trial{trial}"] = handle_response(trial_dictionary=Data_Dictionary[f"trial{trial}"], screen_width=screen_width, screen_height=screen_height)
+
+    Data_Dictionary[f"trial{trial}"]["end_time"] = datetime.now()
+
+csv_log_dir = Logger.create_log(filetype=".csv", log_name=f"{Data_Dictionary['whole_session_data']['pid']}_msit_data")
+Logger.update_log(log_name=csv_log_dir, dictionary_to_write=Data_Dictionary)
+Projector.show_end_message(screen=screen)
