d1e0b5d7c169e641ea040236a4a6fd1f47bf458b
sofheras <158122127+sofheras@users.noreply.github.com>
2024-09-16 10:34:17 -0400

    Update startup_docker.sh

--- startup_docker.sh
+++ startup_docker.sh
@@ -8,6 +8,7 @@ echo "alias scripts='cd /workdir/tasks_run/scripts'" >> ~/.bashrc
 echo "alias samba='cd /workdir/tasks_run/data/sambashare'" >> ~/.bashrc
 echo "alias mock='python /workdir/tasks_run/scripts/other/mock_mri_file_production.py'" >> ~/.bashrc
 echo "alias nfb='python /workdir/tasks_run/scripts/nf_calc_MW.py'" >> ~/.bashrc
+echo "alias msit='python /workdir/tasks_run/scripts/MSIT_NF_MW.py'" >> ~/.bashrc # change name of script to latest version 
 
 source ~/.bashrc
 
