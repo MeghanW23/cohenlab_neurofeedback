fbde62e885b67d886a72bfc10cb17e3e1c712f62
MeghanW23 <wmeghan0809@gmail.com>
2024-09-16 15:25:40 -0400

    edit nfb script

--- tasks_run/scripts/ProjectorMW.py
+++ tasks_run/scripts/ProjectorMW.py
@@ -1,5 +1,5 @@
 import sys
-
+import re
 import pygame
 from typing import Tuple
 import settings
@@ -32,7 +32,6 @@ def get_monitor_info(dictionary: dict) -> Tuple[dict, pygame.Surface]:
     screen: pygame.Surface = pygame.display.set_mode((dictionary["whole_session_data"]["second_monitor_width"], dictionary["whole_session_data"]["second_monitor_height"]), pygame.FULLSCREEN | pygame.NOFRAME)
 
     return dictionary, screen
-
 def show_end_message(screen: pygame.Surface):
     Logger.print_and_log(f"SUBJECT IS DONE. DISPLAYING EXIT MESSAGE FOR {settings.DISPLAY_EXIT_MESSAGE_TIME}")
 
@@ -46,7 +45,6 @@ def show_end_message(screen: pygame.Surface):
     pygame.display.flip()
 
     time.sleep(settings.DISPLAY_EXIT_MESSAGE_TIME)  # show the message on screen for 5 seconds
-
 def show_instructions(screen: pygame.Surface, instructions: list) -> None:
     Logger.print_and_log("Showing Instructions. Task will start when 's' is pressed (scanner key maps to s on mac, so starting the experiment will trigger the task)")
     font: pygame.font.Font = pygame.font.Font(None, settings.INSTRUCT_MESSAGE_FONT_SIZE)
@@ -68,7 +66,6 @@ def show_instructions(screen: pygame.Surface, instructions: list) -> None:
                 return None
             else:
                 time.sleep(0.1)
-
 def initialize_screen(screen: pygame.Surface, instructions: list):
     Logger.print_and_log("TO SHOW INSTRUCTIONS, PLEASE PRESS 'r'.")
     font: pygame.font.Font = pygame.font.Font(None, settings.INSTRUCT_MESSAGE_FONT_SIZE)
@@ -124,7 +121,6 @@ def setup_nfb_icons(dictionary: dict) -> dict:
     dictionary["whole_session_data"]["bg4"] = pygame.transform.scale(pygame.image.load(settingsMW.BACKGROUND_PATH_4).convert(), (dictionary["whole_session_data"]["second_monitor_width"], dictionary["whole_session_data"]["second_monitor_height"]))
 
     return dictionary
-
 def check_nfb_block_setup(dictionary: dict) -> Tuple[dict, int, int]:
     current_block, current_trial = ScriptManager.dict_get_most_recent(dictionary=dictionary, get="both")
 
@@ -140,7 +136,6 @@ def check_nfb_block_setup(dictionary: dict) -> Tuple[dict, int, int]:
         dictionary[current_block]["portal_image"] = dictionary["whole_session_data"]["portal_image"]
 
     return dictionary, current_block, current_trial
-
 def nfb_collision_handler(dictionary: dict, current_block: int, screen: pygame.Surface) -> dict:
     if dictionary[current_block]["rocket_x"] >= (dictionary[current_block]["portal_x"] * 0.9):  # collision
         dictionary[current_block]["collision_count"] += 1
@@ -205,6 +200,21 @@ def nfb_collision_handler(dictionary: dict, current_block: int, screen: pygame.S
             dictionary[current_block]["current_level"] = 4
 
     return dictionary
+def nfb_streak_count(dictionary: dict, current_block: int, current_trial: int, nfb_type: str, screen: pygame.Surface) -> dict:
+    if "streak_counter" not in dictionary[current_block]:
+        dictionary[current_block]["streak_counter"]: int = 0
+
+    if dictionary[current_block][-1][nfb_type] <= dictionary[current_block][current_trial][nfb_type]:
+        dictionary[current_block]["streak_counter"] += 1
+    else:
+        dictionary[current_block]["streak_counter"] = 0
+
+    if dictionary[current_block]["streak_counter"] >= settingsMW.TRIALS_BEFORE_STREAK_REPORT:
+        screen.blit(dictionary["whole_session_data"]["rocket_image_flames"], (dictionary[current_block]["rocket_x"], dictionary["whole_session_data"]["rocket_y"]))
+        screen.blit(dictionary["whole_session_data"]["streak"], (dictionary["whole_session_data"]["second_monitor_width"] // settingsMW.STREAK_LOCATION_DIVISORS[0] - dictionary["whole_session_data"]["streak"].get_width() // settingsMW.STREAK_LOCATION_DIVISORS[1],
+                             dictionary["whole_session_data"]["second_monitor_height"] // settingsMW.STREAK_LOCATION_DIVISORS[2] - dictionary["whole_session_data"]["streak"].get_height() // settingsMW.STREAK_LOCATION_DIVISORS[3]))
+    return dictionary
+
 
 def project_nfb_trial(dictionary: dict, screen: pygame.Surface) -> dict:
     screen.fill((0, 0, 0))
@@ -234,11 +244,19 @@ def project_nfb_trial(dictionary: dict, screen: pygame.Surface) -> dict:
     screen.blit(print_level, (dictionary["whole_session_data"]["second_monitor_width"] // settings.PRINT_LEVEL_LOCATION_DIVISORS[0] - print_level.get_width() // settings.PRINT_LEVEL_LOCATION_DIVISORS[1], dictionary["whole_session_data"]["second_monitor_height"] // settings.PRINT_LEVEL_LOCATION_DIVISORS[2] - print_level.get_height() // settings.PRINT_LEVEL_LOCATION_DIVISORS[3]))
     screen.blit(dictionary[current_block]["portal_image"], (dictionary[current_block]["portal_x"], dictionary[current_block]["portal_y"]))
 
+    nfb_type: str = ""
+    if settings.NFB_FROM_MEAN_ACTIVATION:
+        nfb_type: str = "normalized_mean_activation"
+
+    elif settings.NFB_FROM_RESIDUAL_VALUE:
+        nfb_type: str = "normalized_resid_mean"
+
     # add one to the value so the values are between (0, 2) and not (-1, 1)
     if settings.NFB_FROM_MEAN_ACTIVATION:
-        nfb_value: float = dictionary[current_block][current_trial]["normalized_mean_activation"]
+        nfb_value: float = dictionary[current_block][current_trial][nfb_type]
+
     elif settings.NFB_FROM_RESIDUAL_VALUE:
-        nfb_value: float = dictionary[current_block][current_trial]["normalized_resid_mean"]
+        nfb_value: float = dictionary[current_block][current_trial][nfb_type]
     else:
         Logger.print_and_log("Please Choose What Calculation You Want To Run via the settings script. ex: mean_activation, residuals, etc.")
         sys.exit(1)
@@ -258,12 +276,12 @@ def project_nfb_trial(dictionary: dict, screen: pygame.Surface) -> dict:
         screen.blit(dictionary["whole_session_data"]["rocket_image"], (rocket_x, dictionary["whole_session_data"]["rocket_y"]))
         dictionary[current_block]["rocket_x"] = rocket_x
 
+        dictionary = nfb_streak_count(dictionary=dictionary, current_block=current_block, current_trial=current_block, nfb_type=nfb_type, screen=screen)
         dictionary = nfb_collision_handler(dictionary=dictionary, current_block=current_block, screen=screen)
 
     pygame.display.flip()
 
     return dictionary
-
 def show_fixation_cross(dictionary: dict, screen: pygame.Surface):
     fixation: pygame.Surface = pygame.image.load(settings.FIXATION_PATH)
 
