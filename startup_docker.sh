8d3d56f54b02a92bacff9ac405a1a73730bdd52d
MeghanW23 <wmeghan0809@gmail.com>
2024-09-17 17:03:46 -0400

    add testdir aliases

--- startup_docker.sh
+++ startup_docker.sh
@@ -8,7 +8,9 @@ echo "alias scripts='cd /workdir/tasks_run/scripts'" >> ~/.bashrc
 echo "alias samba='cd /workdir/tasks_run/data/sambashare'" >> ~/.bashrc
 echo "alias mock='python /workdir/tasks_run/scripts/other/mock_mri_file_production.py'" >> ~/.bashrc
 echo "alias nfb='python /workdir/tasks_run/scripts/nf_calc_MW.py'" >> ~/.bashrc
-echo "alias msit='python /workdir/tasks_run/scripts/MSIT_NF_MW.py'" >> ~/.bashrc # change name of script to latest version 
+echo "alias msit='python /workdir/tasks_run/scripts/MSIT_NF_latest.py'" >> ~/.bashrc # change name of script to latest version
+echo "alias testdir='cd /workdir/tasks_run/data/sambashare/test_dir'" >> ~/.bashrc # change name of script to latest version
+echo "alias clear_testdir='rm -rf /workdir/tasks_run/data/sambashare/test_dir/*'" >> ~/.bashrc
 
 source ~/.bashrc
 
