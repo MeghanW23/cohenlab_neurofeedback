8ee7e6589b8f3a9f7d20ebed5b340120f0250a90
MeghanW23 <wmeghan0809@gmail.com>
2024-09-18 15:20:00 -0400

    edit gui

---
+++ tasks_run/scripts/GUI.py
@@ -0,0 +1,126 @@
+import tkinter as tk
+from tkinter import ttk
+import sys
+import os
+import subprocess
+import re
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+nfb_log_dir_path: str = "/Users/meghan/cohenlab_neurofeedback/tasks_run/data/nfb_logs"
+data_dictionary: dict = {}
+def start_nfb():
+    update_gui(task='nfb')
+def start_rifg():
+    update_gui(task='rifg')
+def update_gui(task: str):
+    # Call track_nfb to update the GUI
+    if task == 'nfb':
+        track_nfb()
+    elif task == 'rifg':
+        track_rifg()
+
+    # Schedule the next update
+    root.after(1000, update_gui, task)  # Update every 1000 ms (1 second)
+def get_log_file(log_dir_path: str) -> str:
+    text_files: list = [os.path.join(log_dir_path, file) for file in os.listdir(log_dir_path) if file.endswith(".txt")]
+    most_recent_text_file: str = max(text_files, key=os.path.getmtime)
+    return most_recent_text_file
+def track_value(shell_command_to_get_string: str, re_pattern: str, match_group: int) -> float:
+    result = subprocess.run(shell_command_to_get_string, shell=True, text=True, capture_output=True)
+
+    if result.returncode == 0:
+        full_string = result.stdout
+    else:
+        full_string = result.stderr
+    match = re.search(re_pattern, string=full_string)
+    if match and 'nan' not in match.group(match_group):
+        value: float = float(match.group(match_group))
+
+        return value
+def track_nfb():
+    print("Tracking NFB")
+
+    root.after(0, lambda: (nfb_button.destroy(), rifg_button.destroy()))
+
+    nfb_text_file: str = get_log_file(log_dir_path=nfb_log_dir_path)
+
+    nfb_activation_value: float = track_value(shell_command_to_get_string=f"grep 'Normalized Mean Activation' {nfb_text_file} | tail -n 1",
+                                              re_pattern=r"Normalized Mean Activation:\s*(nan|-?\d*\.\d+)",
+                                              match_group=1)
+
+    nfb_update_label.config(text=f"Normalized ROI Activation: {nfb_activation_value}")
+    nfb_update_label.pack()
+
+    try:  # in case there is no activation value returned
+        nfb_progressbar.config(value=(nfb_activation_value * 100))
+        nfb_progressbar.pack()
+    except Exception as e:
+        pass
+
+    trial_num = track_value(shell_command_to_get_string=f"grep 'Starting Block' {nfb_text_file} | grep Trial | tail -n 1",
+                            re_pattern=r"Starting Block(\d+), Trial (\d+)...",
+                            match_group=2)
+
+
+    nfb_trial_label.config(text=f"Trial Number: {int(trial_num)} of 140")
+    nfb_trial_label.pack()
+
+    try:  # in case there is no trial value returned
+        nfb_trial_progressbar.config(value=((trial_num / 140) * 100))
+        nfb_trial_progressbar.pack()
+    except Exception as e:
+        pass
+
+    if trial_num is not None and nfb_activation_value is not None and nfb_activation_value != 'nan':
+        global data_dictionary
+
+        if "nfb_values" not in data_dictionary:
+            data_dictionary["nfb_values"]: list = [nfb_activation_value]
+        else:
+            data_dictionary["nfb_values"].append(nfb_activation_value)
+
+        if "trial_num" not in data_dictionary:
+            data_dictionary["trial_num"]: list = [trial_num]
+        else:
+            data_dictionary["trial_num"].append(trial_num)
+
+        fig = plot_value(x_axis=data_dictionary["trial_num"], y_axis=data_dictionary["nfb_values"], x_label="Trials", y_label="NFB_Value", plot_title="NFB Value over Trials")
+
+def track_rifg():
+    print("Tracking RIFG")
+
+def plot_value(x_axis: list, y_axis: list, x_label: str, y_label: str, plot_title: str):
+    fig = Figure(figsize=(5, 4), dpi=100)
+    ax = fig.add_subplot(111)
+
+    ax.plot(x_axis, y_axis)
+
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(plot_title)
+
+    return fig
+
+def stop():
+    sys.exit(1)
+
+root = tk.Tk()
+title = ttk.Label(root, text="ADHD Stimulants Task Results Visualizer", font=("Times New Roman", 20, "underline"), foreground="black")
+title.pack()
+
+nfb_button = ttk.Button(root, text="Track NFB Progress", command=start_nfb)
+nfb_button.pack()
+
+rifg_button = ttk.Button(root, text="Track RIFG Progress", command=start_rifg)
+rifg_button.pack()
+
+stop_button = ttk.Button(root, text="Close", command=stop)
+stop_button.pack()
+
+nfb_update_label = ttk.Label(root, font=("Times New Roman", 15))
+nfb_progressbar = ttk.Progressbar(root, length=200, orient='horizontal', mode='determinate')
+
+nfb_trial_label = ttk.Label(root, font=("Times New Roman", 15))
+nfb_trial_progressbar = ttk.Progressbar(root, length=200, orient='horizontal', mode='determinate')
+root.mainloop()
