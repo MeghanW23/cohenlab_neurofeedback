96450792bf7035c8275b5eefddcec5543bf81194
MeghanW23 <wmeghan0809@gmail.com>
2024-09-19 13:42:42 -0400

    edit env variables

--- ssh_e3.sh
+++ ssh_e3.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [ "$USERNAME" = "meghan" ]; then
-    chid="ch246081"
-elif [ "$USERNAME" = "sofiaheras" ]; then
-    chid="ch261487"
-else
-    echo "Your username has not been configured for e3 ssh via this script. Please add your ch id or see meghan"
-    exit 1
-fi
-
-echo "Using CH ID: $chid"
+echo "Using CH ID: '${CHID}'"
 echo "Ssh-ing into E3 ... "
-ssh -XYC ${chid}@e3-login.tch.harvard.edu
+ssh -XYC ${CHID}@e3-login.tch.harvard.edu
--- startup_docker.sh
+++ startup_docker.sh
@@ -2,23 +2,14 @@
 
 # ALIASES
 echo "alias rifg='python /workdir/tasks_run/scripts/rifg_task.py'" >> ~/.bashrc
-
 echo "alias data='cd /workdir/tasks_run/data/'" >> ~/.bashrc
-
 echo "alias logs='cd /workdir/tasks_run/data/logs'" >> ~/.bashrc
-
 echo "alias scripts='cd /workdir/tasks_run/scripts'" >> ~/.bashrc
-
 echo "alias samba='cd /workdir/tasks_run/data/sambashare'" >> ~/.bashrc
-
 echo "alias mock='python /workdir/tasks_run/scripts/other/mock_mri_file_production.py'" >> ~/.bashrc
-
 echo "alias nfb='python /workdir/tasks_run/scripts/nf_calc_MW.py'" >> ~/.bashrc
-
 echo "alias msit='python /workdir/tasks_run/scripts/MSIT_NF_latest.py'" >> ~/.bashrc
-
 echo "alias testdir='cd /workdir/tasks_run/data/sambashare/test_dir'" >> ~/.bashrc
-
 echo "alias clear_testdir='rm -rf /workdir/tasks_run/data/sambashare/test_dir/*'" >> ~/.bashrc
 
 echo "e3() {
@@ -26,7 +17,27 @@ cd /workdir/
 ./ssh_e3.sh
 }" >> ~/.bashrc
 
+# ENV VARIABLES
+if [ "$USERNAME" = "meghan" ]; then
+    CHID="ch246081"
+elif [ "$USERNAME" = "sofiaheras" ]; then
+    CHID="ch261487"
+else
+    echo "Your username/chid has not been configured for e3 ssh via this script. Please add your ch id or see meghan"
+    exit 1
+fi
+
+# Use single quotes to prevent immediate expansion
+echo "export CHID='${CHID}'" >> ~/.bashrc
+
+if [ -n "$USERNAME" ]; then
+    echo "Hello, $USERNAME. Docker container setup is all set. Type 'alias' to see available commands."
+else
+    echo "Docker container setup is all set. Type 'alias' to see available commands."
+fi
+
+# Source the .bashrc file to apply changes
 source ~/.bashrc
 
-echo "Hello, $USERNAME. Docker container setup is all set. Type 'alias' to available commands."
+# Execute any commands passed to the script
 exec "$@"
