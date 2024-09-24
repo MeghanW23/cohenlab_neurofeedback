62a527fd773bc92d7a96d6ed9b207309934c32a4
MeghanW23 <wmeghan0809@gmail.com>
2024-09-24 17:07:07 -0400

    edit tarball script

--- get_venv.sh
+++ get_venv.sh
@@ -1,10 +1,25 @@
-#  gdown --no-cookies "https://drive.google.com/file/d/1tLkdwqhbE40BLnxVN37rU2ku7VlQFZEZ/view?usp=sharing" -O venv.tar.gz
-tarball_path:"/workdir/venv.tar.gz"
+#!/bin/bash
 
+tarball_path="/workdir/venv.tar.gz"
 echo "Creating python virtual environment from tarball"
-if [ ! -d $tarball_path ]; then
+if [ ! -d "$tarball_path" ]; then
   echo "Cannot find path to venv tarball. Attempting to pull from google drive using gdown"
-  gdown --no-cookies "https://drive.google.com/file/d/1tLkdwqhbE40BLnxVN37rU2ku7VlQFZEZ/view?usp=sharing" -O venv.tar.gz
+  command_output=$(gdown --no-cookies "https://drive.google.com/file/d/1tLkdwqhbE40BLnxVN37rU2ku7VlQFZEZ/view?usp=sharing" -O venv.tar.gz)
+  # Check if the command was successful
+  if [ $? -eq 0 ]; then
+    echo "$command_output"
+    echo "Successful Pull:"
+  else
+    echo "Command failed."
+  fi
+  if [ ! -d "$tarball_path" ]; then
+    echo "Could not find tarball after pulling from gdrive"
+    exit 1
+  else
+    tar -xzvf venv.tar.gz
+  fi
 else
-  tar -xzvf filename.tar.gz
+  tar -xzvf venv.tar.gz
+fi
+
 
