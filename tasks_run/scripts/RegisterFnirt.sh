63686056b60100147111e61429a9ed27b55adcac
MeghanW23 <wmeghan0809@gmail.com>
2024-09-26 09:47:40 -0400

    edit fnirt script

--- tasks_run/scripts/RegisterFnirt.sh
+++ tasks_run/scripts/RegisterFnirt.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Function to handle script exit
+handle_error_exit() {
+  echo "An error occurred. Exiting the script."
+  echo "Last command: $last_command"
+  exit 1  # Exit with a non-zero status indicating an error
+}
+
+# Trap any errors and call the handle_exit function
+trap 'last_command=$BASH_COMMAND; handle_error_exit' ERR
+
 echo "Registering ROI mask to participant data via local fnirt script"
 
 set -e
@@ -13,6 +23,7 @@ echo "Using Most Recent DICOM DIR: ${dicom_dir}"
 outdir=$(python -c "from settings import TMP_OUTDIR_PATH; print(TMP_OUTDIR_PATH)")
 echo "Pushing outputted files to: ${outdir}"
 
+# get mni brain
 mni_brain=$(python -c "from settings import MNI_BRAIN_PATH; print(MNI_BRAIN_PATH)")
 echo "Path to MNI Brain: ${mni_brain}"
 
@@ -29,18 +40,21 @@ while true; do
     echo "Ok, Registering ACC Mask"
     roi_mask=$(python -c "from settings import MNI_ACC_MASK_PATH; print(MNI_ACC_MASK_PATH)")
     echo "Using MNI ACC Mask at: $roi_mask"
-
+    mask_type="acc"
     break
+
   elif [ $choice = "2" ]; then
     echo "Ok, Registering Motor Mask"
     roi_mask=$(python -c "from settings import MNI_MOTOR_MASK_PATH; print(MNI_MOTOR_MASK_PATH)")
     echo "Using MNI Motor Mask at: $roi_mask"
+    mask_type="motor"
     break
 
   elif [ $choice = "3" ]; then
     echo "Ok, Registering RIFG Mask"
     roi_mask=$(python -c "from settings import MNI_RIFG_MASK_PATH; print(MNI_RIFG_MASK_PATH)")
     echo "Using MNI RIFG Mask at: $roi_mask"
+    mask_type="rifg"
     break
 
   else
@@ -48,13 +62,37 @@ while true; do
   fi
 done
 
-echo "Running dcm2niix on the dicom dir"
+# get pid and timestamp, then use to make outputted registered subj-space mask path
+while true; do
+  read -p "Enter pid: " pid
+  letter_count=$(echo "$pid" | tr -cd '[:alpha:]' | wc -c)
+  expected_digits=$(echo "$pid" | tr -d 'Pp')
+
+  if [ "$letter_count" -ne 1 ]; then
+    echo "Please follow pid syntax: 'P' followed by digits"
+  elif [[ ! "$pid" == *[Pp]* ]]; then
+    echo "Please follow pid syntax: 'P' followed by digits"
+  elif [[ ! "$expected_digits" =~ ^[0-9]+$ ]]; then
+    echo "Please follow pid syntax: 'P' followed by digits"
+  else
+    echo "Ok, using pid: ${pid}"
+    break
+  fi
+done
+
+timestamp=$(date +"%Y%m%d_%H%M%S")
+
+output_registered_brain_parent_dir=$(python -c "from settings import ROI_MASK_DIR_PATH; print(ROI_MASK_DIR_PATH)")
+output_registered_brain="${output_registered_brain_parent_dir}/${pid}_fnirt_registered_${mask_type}_mask_${timestamp}"
+
+
+echo "Running dcm2niix on the dicom dir ..."
 dcm2niix -o "$outdir" "$dicom_dir"
 
 echo "Cutting off first nifti slice ..."
 output_nii_path=$(ls -tr ${outdir} | grep -E 'nii|nii.gz' | tail -n 1)
 three_dimensional_nifti_path="${outdir}/3d_nifti"
-fslroi "$output_nii_path", "$three_dimensional_nifti_path" 0 -1 0 -1 0 -1 0 1
+fslroi "$output_nii_path" "$three_dimensional_nifti_path" 0 -1 0 -1 0 -1 0 1
 
 echo "Skull-stripping the brain ..."
 ss_three_dimensional_nifti_path="${outdir}/ss_3d_nifti"
@@ -95,6 +133,6 @@ echo "Applying Fnirt ..."
 applywarp \
 --ref="$three_dimensional_nifti_path" \
 --in="$roi_mask" \
---interp==nn \
+--interp=nn \
 --warp="$nonlinear_matrix" \
---out=
+--out="$output_registered_brain"
