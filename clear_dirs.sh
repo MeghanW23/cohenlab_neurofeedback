b458ed39bedc0536cf88fcc6c35c19fbba58ca32
MeghanW23 <wmeghan0809@gmail.com>
2024-09-26 14:30:10 -0400

    edit clear_dirs.sh

--- clear_dirs.sh
+++ clear_dirs.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+echo "This script clears directories that store data not needed past script runtime"
+
+directories_to_check=(
+$(python -c "from settings import TMP_OUTDIR_PATH; print TMP_OUTDIR_PATH"),
+$(python -c "from settings import TMP_OUTDIR_PATH; print TMP_OUTDIR_PATH"),
+
+)
