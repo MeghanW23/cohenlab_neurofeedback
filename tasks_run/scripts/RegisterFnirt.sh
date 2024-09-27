3acbd82d445d1f0f7f71fe2f1221de6e3caa69f9
MeghanW23 <wmeghan0809@gmail.com>
2024-09-27 12:27:46 -0400

    edit register fnirt

--- tasks_run/scripts/RegisterFnirt.sh
+++ tasks_run/scripts/RegisterFnirt.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Function to handle script exit
-handle_error_exit() {
-  echo "An error occurred. Exiting the script."
-  echo "Last command: $last_command"
-  exit 1  # Exit with a non-zero status indicating an error
-}
-
-# Trap any errors and call the handle_exit function
-trap 'last_command=$BASH_COMMAND; handle_error_exit' ERR
-
 echo "Registering ROI mask to participant data via local fnirt script"
 
 set -e
@@ -90,16 +80,17 @@ echo "Running dcm2niix on the dicom dir ..."
 dcm2niix -o "$outdir" "$dicom_dir"
 
 echo "Cutting off first nifti slice ..."
-output_nii_path=$(ls -tr ${outdir} | grep -E 'nii|nii.gz' | tail -n 1)
-three_dimensional_nifti_path="${outdir}/3d_nifti"
+output_nii_filename=$(ls -tr ${outdir} | grep -E 'nii|nii.gz' | tail -n 1)
+output_nii_path="${outdir}/${output_nii_filename}"
+three_dimensional_nifti_path="${outdir}/3d_nifti.nii.gz"
 fslroi "$output_nii_path" "$three_dimensional_nifti_path" 0 -1 0 -1 0 -1 0 1
 
 echo "Skull-stripping the brain ..."
-ss_three_dimensional_nifti_path="${outdir}/ss_3d_nifti"
+ss_three_dimensional_nifti_path="${outdir}/ss_3d_nifti.nii.gz"
 bet "$three_dimensional_nifti_path" "$ss_three_dimensional_nifti_path"
 
 echo "Creating reference brain mask ..."
-func_three_dimensional_mask="${outdir}/func_3d_brain_mask_nifti"
+func_three_dimensional_mask="${outdir}/func_3d_brain_mask_nifti.nii.gz"
 fslmaths "$ss_three_dimensional_nifti_path" -bin "$func_three_dimensional_mask"
 
 echo "Running Flirt ..."
