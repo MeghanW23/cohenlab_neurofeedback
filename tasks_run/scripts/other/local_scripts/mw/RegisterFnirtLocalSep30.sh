377ca3b02e57349785335fc2cf6cbe236bc87697
MeghanW23 <wmeghan0809@gmail.com>
2024-09-30 14:22:38 -0400

    test fnirt locally, edit fnirt registration, edit .gitignore

--- tasks_run/scripts/other/local_scripts/mw/RegisterFnirtLocalSep30.sh
+++ tasks_run/scripts/other/local_scripts/mw/RegisterFnirtLocalSep30.sh
@@ -5,16 +5,16 @@ echo "Registering ROI mask to participant data via local fnirt script"
 set -e
 
 # get most recent dicom dir
-samba_dir=$(python -c "from settings import SAMBASHARE_DIR_PATH; print(SAMBASHARE_DIR_PATH)")
+samba_dir="/Users/meghan/cohenlab_neurofeedback/tasks_run/data/sambashare/"
 dicom_dir="${samba_dir}/$(ls -tr ${samba_dir} | tail -n 1)"
 echo "Using Most Recent DICOM DIR: ${dicom_dir}"
 
 # push unnecessary files to outdir
-outdir=$(python -c "from settings import TMP_OUTDIR_PATH; print(TMP_OUTDIR_PATH)")
+outdir="/Users/meghan/cohenlab_neurofeedback/tasks_run/tmp_outdir/"
 echo "Pushing outputted files to: ${outdir}"
 
 # get mni brain
-mni_brain=$(python -c "from settings import MNI_BRAIN_PATH; print(MNI_BRAIN_PATH)")
+mni_brain="/Users/meghan/cohenlab_neurofeedback/tasks_run/data/localizer_data/mni_brain.nii.gz"
 echo "Path to MNI Brain: ${mni_brain}"
 
 # get mni roi mask via experimenter input
@@ -28,21 +28,21 @@ while true; do
 
   if [ $choice = "1" ]; then
     echo "Ok, Registering ACC Mask"
-    roi_mask=$(python -c "from settings import MNI_ACC_MASK_PATH; print(MNI_ACC_MASK_PATH)")
+    roi_mask="/Users/meghan/cohenlab_neurofeedback/tasks_run/data/localizer_data/mni_acc_mask.nii.gz"
     echo "Using MNI ACC Mask at: $roi_mask"
     mask_type="acc"
     break
 
   elif [ $choice = "2" ]; then
     echo "Ok, Registering Motor Mask"
-    roi_mask=$(python -c "from settings import MNI_MOTOR_MASK_PATH; print(MNI_MOTOR_MASK_PATH)")
+    roi_mask="/Users/meghan/cohenlab_neurofeedback/tasks_run/data/localizer_data/mni_motor_mask.nii.gz"
     echo "Using MNI Motor Mask at: $roi_mask"
     mask_type="motor"
     break
 
   elif [ $choice = "3" ]; then
     echo "Ok, Registering RIFG Mask"
-    roi_mask=$(python -c "from settings import MNI_RIFG_MASK_PATH; print(MNI_RIFG_MASK_PATH)")
+    roi_mask="/Users/meghan/cohenlab_neurofeedback/tasks_run/data/localizer_data/mni_rIFG_mask.nii.gz"
     echo "Using MNI RIFG Mask at: $roi_mask"
     mask_type="rifg"
     break
@@ -72,7 +72,7 @@ done
 
 timestamp=$(date +"%Y%m%d_%H%M%S")
 
-output_registered_brain_parent_dir=$(python -c "from settings import ROI_MASK_DIR_PATH; print(ROI_MASK_DIR_PATH)")
+output_registered_brain_parent_dir="/Users/meghan/cohenlab_neurofeedback/tasks_run/data/localizer_data/subj_space_masks/"
 output_registered_brain="${output_registered_brain_parent_dir}/${pid}_fnirt_registered_${mask_type}_mask_${timestamp}"
 
 
