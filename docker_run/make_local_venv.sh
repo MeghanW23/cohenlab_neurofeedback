accd60e934b1512f6b0ba3f58323ad169bee0fdc
samba_user <samba_user@OSXLAP09562.local>
2024-11-20 11:45:28 -0500

    edit local_venv maker

--- docker_run/make_local_venv.sh
+++ docker_run/make_local_venv.sh
@@ -6,8 +6,22 @@ set -e
 
 # Check if variables are set
 if [ -z "$LOCAL_VENV_DIR_PATH" ] || [ -z "$LOCAL_VENV_REQUIREMENTS_FILE" ]; then
-  echo "Error: Please set LOCAL_VENV_DIR_PATH and LOCAL_VENV_REQUIREMENTS_FILE before running this script."
-  exit 1
+  settings_path="$(dirname $(dirname $(realpath "$0")))/tasks_run/scripts/settings.py"
+  echo "Getting variables LOCAL_VENV_DIR_PATH and LOCAL_VENV_REQUIREMENTS_FILE from: " 
+  echo "$settings_path"
+
+  LOCAL_VENV_REQUIREMENTS_FILE=$(python ${settings_path} LOCAL_VENV_REQUIREMENTS_FILE -s)
+  LOCAL_VENV_DIR_PATH=$(python ${settings_path} LOCAL_VENV_DIR_PATH -s)
+  echo "Input Paths: "
+  echo "LOCAL_VENV_REQUIREMENTS_FILE: ${LOCAL_VENV_REQUIREMENTS_FILE}"
+  echo "LOCAL_VENV_DIR_PATH: ${LOCAL_VENV_DIR_PATH}"
+  
+  if [ ! -f "$LOCAL_VENV_REQUIREMENTS_FILE" ]; then
+    echo "LOCAL_VENV_REQUIREMENTS_FILE does not exist"
+    exit 1
+  fi
+
+
 fi
 
 # Create the conda environment
@@ -18,8 +32,11 @@ source activate "$LOCAL_VENV_DIR_PATH"
 
 # Install requirements
 pip install -r "$LOCAL_VENV_REQUIREMENTS_FILE"
+pip install dcm2niix
 
 echo "All set! Please note that FSL will need to be downloaded manually through their website:"
 echo "https://fsl.fmrib.ox.ac.uk/fsl/docs/#/" 
 
 echo "To activate the virtual env, you can type: 'conda activate ${LOCAL_VENV_DIR_PATH}'"
+echo "or:"
+echo "conda activate local_venv"
