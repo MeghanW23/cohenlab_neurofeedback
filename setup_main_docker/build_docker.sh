92ac9142ed92baed98f613925e05da65b04f2ab4
MeghanW23 <wmeghan0809@gmail.com>
2024-12-10 11:31:30 -0500

    edit main docker Dockerfile and build_docker.sh

--- setup_main_docker/build_docker.sh
+++ setup_main_docker/build_docker.sh
@@ -2,6 +2,14 @@
 
 echo "To build a multiarch docker image, we will need to push the created image to your dockerhub account."
 read -p "Enter your dockerfile username: " docker_username
+echo "This project's docker image uses the name 'nfb_docker'. If this is not true for you, please swap out all mentions of 'nfb_docker' in the project to your project name."
+read -p "Name the image: " docker_image
+
+echo "Tips for docker building: "
+echo "  1. If you are using a VPN, it is recommended you disconnect."
+echo "      (VPNs can cause intermittent network issues which may cause network interruptions when pushing the image to your dockerhub account.)" 
+echo "  2. Running this script with 'sudo' may be necessary."
+read -p "Press enter to continue. "
 
 # Create docker builder
 if ! docker buildx inspect multi-arch > /dev/null 2>&1; then
@@ -14,9 +22,9 @@ fi
 
 # Build docker image
 echo "Building Docker image and pushing two tags (1.0 and latest) to dockerhub..."
-docker buildx build --platform linux/amd64,linux/arm64 -t "$docker_username"/nfb_docker:1.0 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t "$docker_username"/${docker_image}:1.0 --push .
 
-docker buildx build --platform linux/amd64,linux/arm64 -t meghanwalsh/nfb_docker:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t meghanwalsh/${docker_image}:latest --push .
 
 echo "Removing builder now ..."
 docker buildx rm multiarch_docker
