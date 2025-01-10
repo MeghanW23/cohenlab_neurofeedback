2af2d714e6cbfc5c5efe47ec568c1478135d7d3d
MeghanW23 <wmeghan0809@gmail.com>
2025-01-10 15:18:33 -0500

    edit open_vnc script to tigerVNC

--- docker_run/vnc_server_material/open_vnc.sh
+++ docker_run/vnc_server_material/open_vnc.sh
@@ -6,8 +6,8 @@ port=$2
 while true; do
     nc -zv "$ip" "$port" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        # /Applications/TigerVNC\ Viewer\ 1.14.1.app/Contents/MacOS/TigerVNC\ Viewer ${ip}:${port} > /dev/null 2>&1
-        open vnc://localhost:5999
+        /Applications/TigerVNC\ Viewer\ 1.14.1.app/Contents/MacOS/TigerVNC\ Viewer ${ip}:${port} > /dev/null 2>&1
+        # open vnc://localhost:5999
         exit
     fi
     sleep 0.1
