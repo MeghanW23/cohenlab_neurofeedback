c981365c99b20e47c868c1e16b7e89760db0f737
sofheras <sofiaheras@osxlap11684.tch.harvard.edu>
2025-01-14 12:32:16 -0500

    merge

--- docker_run/vnc_server_material/open_vnc.sh
+++ docker_run/vnc_server_material/open_vnc.sh
@@ -6,7 +6,8 @@ port=$2
 while true; do
     nc -zv "$ip" "$port" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        open vnc://"$ip":"$port"
+        /Applications/TigerVNC\ Viewer\ 1.14.1.app/Contents/MacOS/TigerVNC\ Viewer ${ip}:${port} > /dev/null 2>&1
+        # open vnc://"$ip":"$port"
         exit
     fi
     sleep 0.1
