5f04cd0907b62129b6c654e23096ae955153e8fa
MeghanW23 <wmeghan0809@gmail.com>
2025-01-15 08:48:03 -0500

    update docker_run/run_interactively.sh

--- docker_run/run_interactively.sh
+++ docker_run/run_interactively.sh
@@ -1,9 +1,12 @@
 settings_script_path="$(dirname $(dirname "$(realpath "$0")"))/tasks_run/scripts/settings.py"
 
-sudo docker run -it --rm \
-  -e DISPLAY=host.docker.internal:0 \
+sudo docker run -it \
+  -p 5999:5999 \
+  -e TZ="$(python "$settings_script_path" TZ -s)" \
+  -e DISPLAY=:99 \
+  -e USER="$USER" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "$(python "$settings_script_path" PROJECT_DIRECTORY -s)":"$(python "$settings_script_path" docker PROJECT_DIRECTORY -s)" \
   -v "$(python "$settings_script_path" LOCAL_SAMBASHARE_DIR_PATH -s)":"$(python "$settings_script_path" docker SAMBASHARE_DIR_PATH -s)" \
-  meghanwalsh/nfb_docker:latest \
-  /bin/bash
+  --entrypoint /bin/bash \
+  meghanwalsh/nfb_docker:latest
