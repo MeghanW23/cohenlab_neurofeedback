4c8e5325a890d9220b774acdeb8b3fe335422703
MeghanW23 <wmeghan0809@gmail.com>
2025-02-14 12:17:58 -0500

    add java gui x11 forwarding material

--- other/java-gui/test_docker_java_gui/docker_run_command.sh
+++ other/java-gui/test_docker_java_gui/docker_run_command.sh
@@ -1,4 +1,5 @@
 docker run -it --rm \
 -v  /Users/meghan/cohenlab_neurofeedback/other/java-gui/test_docker_java_gui:/test_docker_java_gui \
 -e DISPLAY=10.23.229.77:0 \
-java_test_gui /bin/bash
+java_test_gui /bin/bash 
+ 
