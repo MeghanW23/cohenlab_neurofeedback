e3c0de68f42b0e68912399543d2425966c325982
MeghanW23 <wmeghan0809@gmail.com>
2025-03-18 12:24:10 -0400

    edit run_gui.sh

--- nfb_gui/run_gui.sh
+++ nfb_gui/run_gui.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ssh meghan@192.168.1.233 'open /Applications/TigerVNC\ Viewer\ 1.14.1.app/'
+ssh meghan@${ipconfig getifaddr en0} 'open /Applications/TigerVNC\ Viewer\ 
+1.14.1.app/'
 mvn exec:java -Dexec.mainClass="com.cohenlab.App"
 
