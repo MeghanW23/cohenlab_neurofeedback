e791d45c3a8f47132adfbe26ecf7108e08f83467
MeghanW23 <wmeghan0809@gmail.com>
2025-03-19 13:18:47 -0400

    add paths from settings to gui scripts

--- nfb_gui/gui_startup.sh
+++ nfb_gui/gui_startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-Xvfb :98 -screen 0 800x800x24 > "xvfb.log" 2>&1 &
-x11vnc -display :98 -geometry 800x800 -forever  -verbose -nopw -rfbport 5998 > "x11vnc.log" 2>&1 &
+Xvfb :98 -screen 0 800x800x24 > "$GUI_XVFB_LOG" 2>&1 &
+x11vnc -display :98 -geometry 800x800 -forever  -verbose -nopw -rfbport 5998 > "$GUI_X11_LOG" 2>&1 &
 
 cd "$GUI_DIR"
 
 "${PATH_TO_RUN_GUI_SCRIPT}"
+
+exit
--- nfb_gui/run_gui.sh
+++ nfb_gui/run_gui.sh
@@ -2,3 +2,4 @@
 ssh ${USER}@${HOST_IP} 'open /Applications/TigerVNC\ Viewer\ 1.14.1.app/'
 mvn exec:java -Dexec.mainClass="com.cohenlab.App"
 
+exit
