872b628899085db290c441df37cd4f8715e653ed
MeghanW23 <wmeghan0809@gmail.com>
2025-03-19 13:14:07 -0400

    edit gui

--- nfb_gui/docker_run_command.sh
+++ nfb_gui/docker_run_command.sh
@@ -7,6 +7,9 @@ docker run --rm -it \
 -e DISPLAY=:98 \
 -e HOST_IP="$(ipconfig getifaddr en0)" \
 -e USER="${USER}" \
+-e PATH_TO_RUN_GUI_SCRIPT="$(python "$SETTINGSPATH" docker GUI_RUN_SCRIPT -s)" \
+-e GUI_POM_PATH="$(python "$SETTINGSPATH" docker GUI_POM_PATH -s)" \
+-e GUI_DIR="$(python "$SETTINGSPATH" docker GUI_DIR -s)" \
 -v $(python "$SETTINGSPATH" PROJECT_DIRECTORY -s):/workdir \
 meghanwalsh/nfb_java_gui:latest \
 "$(python "$SETTINGSPATH" docker GUI_STARTUP_SCRIPT -s)" 
