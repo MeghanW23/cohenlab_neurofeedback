d01bb559cb21c84f7294e59d281fd748ed7860c8
MeghanW23 <wmeghan0809@gmail.com>
2025-03-26 13:50:48 -0400

    edit gui running script

--- nfb_gui/run_gui.sh
+++ nfb_gui/run_gui.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # ssh ${USER}@${HOST_IP} 'open /Applications/TigerVNC\ Viewer\ 1.14.1.app/'
-mvn exec:java -Dexec.mainClass="com.cohenlab.App"
-
-exit
+mvn exec:java -Dexec.mainClass="com.cohenlab.App" && exit
